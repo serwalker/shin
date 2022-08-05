bf5dcd66cb658c33de513eb03e93e2b1380223a9
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2022-08-05 15:37:14 +0700

    CtrlUB

---
+++ CtrlUB/__main__.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2022 CtrlUB
+#
+# This file is a part of < https://github.com/kennedy-ex/CtrlUB/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/kennedy-ex/CtrlUB/blob/main/LICENSE/>.
+#
+
+
+import sys
+import importlib
+from pyrogram import idle
+from uvloop import install
+from CtrlUB.config import BOT_TOKEN
+
+from CtrlUB.version import __version__
+from CtrlUB import app, bot, BOTLOG_CHATID, LOOP, aiosession
+from CtrlUB.helpers import *
+from CtrlUB.logging import LOGGER
+
+
+async def main():
+    if BOT_TOKEN:
+            await app.start()
+    await bot.start()
+    LOGGER("CtrlUB").info(f"Bot v{__version__} is actived! 🔥")
+    await idle()
+    await aiosession.close()
+
+if __name__ == "__main__":
+    install()
+    git()
+    heroku()
+    LOOP.run_until_complete(main())
